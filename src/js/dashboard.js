43b2cb20c03a01051fdc64a6b8488285e47a136f
IanAguilera24 <aguilera.marquez.ianalexis@gmail.com>
2024-06-22 15:43:22 -0600

    1st commit

---
+++ src/js/dashboard.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Aquí puedes añadir funcionalidades adicionales con JavaScript si lo necesitas.
+    console.log("Página cargada y lista.");
+});
